92740300efc913c1e4d32acc1cf500a1199c4b2b
Tiago Lubiana <tiagolubiana@hotmail.com>
2018-09-21 17:35:00 -0300

    using ggplot2 for su_plot

--- R/su_plot.R
+++ R/su_plot.R
@@ -20,9 +20,25 @@
 
 
 su_plot <- function(x, y) {
+  require(ggplot2)
+  require(gridExtra)
   x <- data.frame(x)
   su_ic <- apply(x, 2, function(xx, yy) {
     SU(xx, yy)
   }, y)
+  su_ic <- as.data.frame(su_ic)
   hist(su_ic)
+  p1 <- ggplot(su_ic, (aes(x=su_ic)))+
+    geom_histogram(binwidth= 0.008) +
+    xlab('Correlation by Symmetrical Uncertainty to target') +
+    ylab('Variable counts') +
+    ggtitle('Histogram of SU values of each variable to the target classes')
+
+  p2 <- ggplot(su_ic, (aes(y=su_ic)))+
+    geom_boxplot(outlier.alpha = 0.5) +
+    ylab('Correlation by Symmetrical Uncertainty to target') +
+    ggtitle('Boxplot of SU values of each variable to the target classes') +
+    theme(axis.text.x=element_blank())
+
+  grid.arrange(p1, p2, nrow = 1)
 }
