ecbf62f852039a65133819e758aff6f5b0964aad
Tiago Lubiana <tiagolubiana@hotmail.com>
2019-03-25 20:39:31 -0300

    exporting functions

--- R/get_su.R
+++ R/get_su.R
@@ -2,13 +2,12 @@
 #'
 #' This functions runs symmetrical uncertainty for a feature table and a class, returning
 #' the scores of symmetrical uncertainty for all features
+#'
 #' @param x A table of features (observations in rows, variables in columns)
 #' @param y A target vector, factor containing classes of the observations. Note: the
 #' observations must be in the same order as the parameter x.
 #' @return A dataframe containing the SU values for each feature
 #' @export
-#' @examples
-#'
 #' @examples
 #' data(scDengue)
 #' exprs <- SummarizedExperiment::assay(scDengue, 'logcounts')
