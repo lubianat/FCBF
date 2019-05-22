89e5a07f3eb4e341413629dde916478bd73eb471
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2019-05-22 15:36:13 -0300

    Changing zzz.R file to solve bugs

--- R/zzz.R
+++ R/zzz.R
@@ -1,13 +1,28 @@
-.onAttach <- function(libname, pkgname){
-  options("pboptions" = list(
+.onLoad <- function(libname, pkgname){
+  opts <- list(
     type = if (interactive()) "timer" else "none",
-    char = "-",
+    char = "+",
     txt.width = 50,
     gui.width = 300,
     style = 3,
     initial = 0,
     title = "R progress bar",
     label = "",
-    nout = 100L))
+    nout = 100L,
+    min_time = 0,
+    use_lb = FALSE)
+  optsx <- getOption("pboptions")
+  if (!is.null(optsx)) {
+    for (i in intersect(names(opts), names(optsx)))
+      opts[[i]] <- optsx[[i]]
+    for (i in setdiff(names(optsx), names(opts)))
+      opts[[i]] <- optsx[[i]]
+  }
+  options("pboptions" = opts)
+  invisible(NULL)
+}
+
+.onUnload <- function(libpath){
+  options("pboptions" = NULL)
   invisible(NULL)
 }
