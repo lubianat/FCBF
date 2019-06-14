91f3f027772fbe296acb7065eedd38ace379f0b6
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2019-06-14 17:46:43 -0300

    Splitting discretizations in a new file and adding possibilities for different unsupervised discretizations

--- R/discretize_exprs.R
+++ R/discretize_exprs.R
@@ -1,6 +1,7 @@
 #' @importMethodsFrom SummarizedExperiment assay
 NULL
 
+
 #' discretize_exprs
 #' Simple discretizing of gene expression
 #'
@@ -13,10 +14,22 @@ NULL
 #'
 #' @param expression_table  A previously normalized expression table
 #' Note: this might drastically change the number of selected features.
+#' @param method Method applied to all genes for discretization. Methods available: "varying_width"
+#'  (Varying width binarization, default, described in function description. Modulated by the number_of_bins param),
+#' "mean" (Split in ON/OFF by each gene mean expression),
+#' "median" (Split in ON/OFF by each gene median expression),
+#' "mean_sd"(Split in low/medium/high by each assigning "medium" to the interval between mean +- standard_deviation.
+#' Modulated by the alpha param, which enlarges (>1) or shrinks (<1) the "medium" interval. ),
+#' ),
+#' "kmeans"(Split in different groups by the kmeans algorithm. As many groups as specified by the centers param) and
+#' "min_max_%" (Similat to the "varying width", a binarization threshold in a % of the min-max range is set. (minmax% param))
 #' @param number_of_bins Number of equal-width bins for discretization.
 #' Note: it is a binary discretization, with the
 #' first bin becoming one class ('low') and the other bins, another class ('high').
 #' Defaults to 3.
+#' @param alpha Modulator for the "mean_sd" method.Enlarges (>1) or shrinks (<1) the "medium" interval. Defaults to 1.
+#' @param centers Modulator for the "kmeans" method. Defaults to 3.
+#' @param min_max_cutoff <- Modulator for the "min_max_%" method. Defaults to 0.25.
 #' @return A data frame with the discretized features in the same order as previously
 #' @export
 #' @examples
@@ -26,26 +39,29 @@ NULL
 #' head(discrete_expression[,1:4])
 
 
-discretize_exprs <- function(expression_table, number_of_bins = 3) {
+discretize_exprs <- function(expression_table, number_of_bins = 3, method = "varying_width", alpha = 1, centers = 3, min_max_cutoff = 0.25) {
+
+  if (method == "varying_width"){
+    .split_fun <-.split_vector_in_two_varying_width
+  } else   if (method == "mean"){
+    .split_fun <-.split_vector_in_two_by_mean
+  }  else   if (method == "median"){
+    .split_fun <-.split_vector_in_two_by_median
+  } else   if (method == "mean_sd"){
+    .split_fun <-.split_vector_in_three_by_mean_sd
+    formals(.split_fun) <- alpha
+  } else   if (method == "kmeans"){
+    .split_fun <-.split_vector_by_kmeans
+    formals(.split_fun) <- centers
+  }  else   if (method == "min_max_%"){
+    .split_fun <-.split_vector_in_two_by_min_max_thresh
+    formals(.split_fun) <- centers
+  }
+
+
   discrete_expression <-
-    apply(expression_table, 1, split_vector_in_two, n_of_bins = number_of_bins)
+    apply(expression_table, 1, .split_fun, n_of_bins = number_of_bins)
   discrete_expression <- as.data.frame(t(discrete_expression))
   rownames(discrete_expression) <- rownames(expression_table)
   return(discrete_expression)
 }
-
-
-split_vector_in_two <-
-  function(gene_expression_across_samples,
-           n_of_bins = 3) {
-    gene_expression_across_samples <-
-      as.numeric(gene_expression_across_samples)
-    max_expression = max(gene_expression_across_samples)
-    min_expression = min(gene_expression_across_samples)
-    break_size = (max_expression - min_expression) / n_of_bins
-    return(ifelse(
-      gene_expression_across_samples < (min_expression + break_size),
-      'low',
-      'high'
-    ))
-  }
--- R/discretize_gene_supervised.R
+++ R/discretize_gene_supervised.R
@@ -2,77 +2,7 @@
 #' @importFrom stats kmeans median sd
 NULL
 
-.split_vector_in_two_by_mean <-
-  function(gene_expression_across_samples) {
-    gene_expression_across_samples <-
-      as.numeric(gene_expression_across_samples)
-    return(ifelse(
-      gene_expression_across_samples < mean(gene_expression_across_samples),
-      'low',
-      'high'
-    ))
-  }
-
-.split_vector_in_two_by_median <-
-  function(gene_expression_across_samples) {
-    gene_expression_across_samples <-
-      as.numeric(gene_expression_across_samples)
-    return(ifelse(
-      gene_expression_across_samples < median(gene_expression_across_samples),
-      'low',
-      'high'
-    ))
-  }
-
-
-.split_vector_by_kmeans <-
-  function(gene_expression_across_samples,
-           centers) {
-    gene_expression_across_samples <-
-      as.numeric(gene_expression_across_samples)
-    tryCatch(
-      return(as.factor(
-        kmeans(gene_expression_across_samples, centers = centers)$cluster
-      )),
-
-      error = function(e) {
-        return(as.factor(gene_expression_across_samples * 0))
-      }
-    )
-  }
-
-.split_vector_in_three_by_mean_sd <-
-  function(gene_expression_across_samples, alpha = 1) {
-    gene_expression_across_samples <-
-      as.numeric(gene_expression_across_samples)
-    avg <- mean(gene_expression_across_samples)
-    modified_sd <- sd(gene_expression_across_samples) * alpha
-    x <-
-      ifelse(
-        gene_expression_across_samples < (avg - modified_sd),
-        'low',
-        ifelse(
-          gene_expression_across_samples < (avg + modified_sd),
-          'medium',
-          'high'
-        )
-      )
-  }
-
-.split_vector_in_two_by_vw <-
-  function(gene_expression_across_samples,
-           cutoff) {
-    gene_expression_across_samples <-
-      as.numeric(gene_expression_across_samples)
-    max_expression = max(gene_expression_across_samples)
-    min_expression = min(gene_expression_across_samples)
-    break_size = (max_expression - min_expression) * cutoff
-    return(ifelse(
-      gene_expression_across_samples < (min_expression + break_size),
-      'low',
-      'high'
-    ))
-  }
+source('R/discretization_methods.R')
 
 #' discretize_gene_supervised
 #'
@@ -95,20 +25,19 @@ NULL
 #' ".split_vector_by_kmeans",
 #' ".split_vector_in_three_by_mean_sd",
 #' ".split_vector_in_two_by_vw")
-
 #' @param sd_alpha Parameter for adusting the 'medium' level of the mean +- sd discretization. Defaults to sd_alpha = c(0.75, 1, 1.25))
 #' @export
 #' @return A data frame with the discretized features in the same order as previously
 #' @examples
-#' data(scDengue)
-#' exprs <- as.data.frame(SummarizedExperiment::assay(scDengue, 'logcounts'))
-#' gene <- exprs['ENSG00000166825',]
-#' infection <- SummarizedExperiment::colData(scDengue)
-#' target <- infection$infection
-#' set.seed(3)
-#' discrete_expression <- as.data.frame(discretize_gene_supervised(gene, target))
-#' table(discrete_expression)
-
+#'  data(scDengue)
+#'  exprs <- as.data.frame(SummarizedExperiment::assay(scDengue, 'logcounts'))
+#'  gene <- exprs['ENSG00000166825',]
+#'  infection <- SummarizedExperiment::colData(scDengue)
+#'  target <- infection$infection
+#'  set.seed(3)
+#'  discrete_expression <- as.data.frame(discretize_gene_supervised(gene, target))
+#'  table(discrete_expression)
+#'
 discretize_gene_supervised <-
   function(gene,
            target,
@@ -118,7 +47,7 @@ discretize_gene_supervised <-
              ".split_vector_in_two_by_mean",
              ".split_vector_by_kmeans",
              ".split_vector_in_three_by_mean_sd",
-             ".split_vector_in_two_by_vw"
+             ".split_vector_in_two_by_min_max_thresh"
            )
            ,
            vw_params = c(0.25, 0.5, 0.75),
@@ -130,7 +59,7 @@ discretize_gene_supervised <-
     disc_top <- 'none'
     vec_top <- gene
     for (disc in discs) {
-      if (length(grep('vw', disc)) != 0) {
+      if (length(grep('min_max_thresh', disc)) != 0) {
         f <- get(disc)
         for (param in vw_params) {
           discretized_gene <- f(gene, param)
