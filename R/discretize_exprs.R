c3f8a4f13f74f6862cc5ea48c430e76cd269d538
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2019-06-14 21:12:05 -0300

    Adding Gaussian Mixture Model discretization

--- R/discretize_exprs.R
+++ R/discretize_exprs.R
@@ -22,7 +22,8 @@ NULL
 #' Modulated by the alpha param, which enlarges (>1) or shrinks (<1) the "medium" interval. ),
 #' ),
 #' "kmeans"(Split in different groups by the kmeans algorithm. As many groups as specified by the centers param) and
-#' "min_max_%" (Similat to the "varying width", a binarization threshold in a % of the min-max range is set. (minmax% param))
+#' "min_max_%" (Similat to the "varying width", a binarization threshold in a % of the min-max range is set. (minmax% param)),
+#' "GMM" (A Gaussian Mixture Model as implemented by the package mclust, trying to fit 2:5 Gaussians)
 #' @param number_of_bins Number of equal-width bins for discretization.
 #' Note: it is a binary discretization, with the
 #' first bin becoming one class ('low') and the other bins, another class ('high').
@@ -30,38 +31,59 @@ NULL
 #' @param alpha Modulator for the "mean_sd" method.Enlarges (>1) or shrinks (<1) the "medium" interval. Defaults to 1.
 #' @param centers Modulator for the "kmeans" method. Defaults to 3.
 #' @param min_max_cutoff <- Modulator for the "min_max_%" method. Defaults to 0.25.
+#' @param progress_bar Enables a progress bar for the discretization. Defaults to TRUE.
 #' @return A data frame with the discretized features in the same order as previously
+#' @import mclust
+#' @import pbapply
 #' @export
 #' @examples
 #' data(scDengue)
 #' exprs <- SummarizedExperiment::assay(scDengue, 'logcounts')
 #' discrete_expression <- as.data.frame(discretize_exprs(exprs))
-#' head(discrete_expression[,1:4])
+#' head(discrete_expression[, 1:4])
 
+discretize_exprs <-
+  function(expression_table,
+           number_of_bins = 3,
+           method = "varying_width",
+           alpha = 1,
+           centers = 3,
+           min_max_cutoff = 0.25,
+           progress_bar = TRUE) {
+    if (method == "varying_width") {
+      .split_fun <- .split_vector_in_two_varying_width
+      formals(.split_fun)[2] <- number_of_bins
+    } else   if (method == "mean") {
+      .split_fun <- .split_vector_in_two_by_mean
+    }  else   if (method == "median") {
+      .split_fun <- .split_vector_in_two_by_median
+    } else   if (method == "mean_sd") {
+      .split_fun <- .split_vector_in_three_by_mean_sd
+      formals(.split_fun)[2] <- alpha
+    } else   if (method == "kmeans") {
+      .split_fun <- .split_vector_by_kmeans
+      formals(.split_fun)[2] <- centers
+    }  else   if (method == "min_max_%") {
+      .split_fun <- .split_vector_in_two_by_min_max_thresh
+      formals(.split_fun)[2] <- min_max_cutoff
+    } else   if (method == "GMM") {
+      .split_fun <- function(X) {
+        sink('aux')
+        fit <- Mclust(X, G = 2:5, warn = FALSE)
+        sink(NULL)
+        return(fit$classification)
+      }
+    }
 
-discretize_exprs <- function(expression_table, number_of_bins = 3, method = "varying_width", alpha = 1, centers = 3, min_max_cutoff = 0.25) {
+    if (progress_bar){
+      discrete_expression <-
+        pbapply(expression_table, 1, .split_fun)
+    } else {
+      discrete_expression <-
+        apply(expression_table, 1, .split_fun)
+    }
 
-  if (method == "varying_width"){
-    .split_fun <-.split_vector_in_two_varying_width
-  } else   if (method == "mean"){
-    .split_fun <-.split_vector_in_two_by_mean
-  }  else   if (method == "median"){
-    .split_fun <-.split_vector_in_two_by_median
-  } else   if (method == "mean_sd"){
-    .split_fun <-.split_vector_in_three_by_mean_sd
-    formals(.split_fun) <- alpha
-  } else   if (method == "kmeans"){
-    .split_fun <-.split_vector_by_kmeans
-    formals(.split_fun) <- centers
-  }  else   if (method == "min_max_%"){
-    .split_fun <-.split_vector_in_two_by_min_max_thresh
-    formals(.split_fun) <- centers
+    discrete_expression <- as.data.frame(t(discrete_expression))
+    rownames(discrete_expression) <- rownames(expression_table)
+    return(discrete_expression)
   }
-
-
-  discrete_expression <-
-    apply(expression_table, 1, .split_fun, n_of_bins = number_of_bins)
-  discrete_expression <- as.data.frame(t(discrete_expression))
-  rownames(discrete_expression) <- rownames(expression_table)
-  return(discrete_expression)
-}
