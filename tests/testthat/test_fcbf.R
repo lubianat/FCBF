dde1825f2826ac2821418d6cbab475a4ced8f83d
lubianat <tiago.lubiana.alves@usp.br>
2020-04-29 09:17:17 -0300

    setting appropriate tests for fcbf

--- tests/testthat/test_fcbf.R
+++ tests/testthat/test_fcbf.R
@@ -1,17 +1,51 @@
 library(FCBF)
 library(testthat)
-data("single_cell_dengue_exprs")
-data("single_cell_dengue_annot")
+
+
+# Variables for testing: 
+
+factor_a = as.factor(c("OFF", "OFF","OFF","OFF","OFF",     "ON", "ON","ON", "ON", "ON"))
+factor_b = as.factor(c("OFF", "OFF","OFF","OFF","OFF",      "OFF","ON", "ON","ON", "ON"))
+factor_c = as.factor(c("OFF", "OFF","OFF","OFF","OFF",      "OFF","OFF", "ON","ON", "ON"))
+factor_d = as.factor(rep("ON", 10))
+
+mock_feature_table = data.frame(A = factor_a, 
+                                   B = factor_b,
+                                   B2 = factor_b,
+                                   B3 = factor_b,
+                                   C = factor_c,
+                                   C2 = factor_c,
+                                   C3 = factor_c,
+                                   D = factor_d)
+
+# For FCBF, genes are in columns, and cells in rows
+mock_expression_table = as.data.frame(t(mock_feature_table))
+
+
+mock_cells = c()
+for (n in 1:10){
+  mock_cells = c(mock_cells,   paste("cell",n))
+}
+
+colnames(mock_expression_table) = mock_cells
+
+
+mock_cell_class = as.factor(c(rep("monocyte", 5), rep("B cell", 5)))
 
 
 test_that("fcbf works properly", {
-  expect_error(fcbf(single_cell_dengue_exprs, single_cell_dengue_annot))
-  expect_error(fcbf(t(single_cell_dengue_exprs, single_cell_dengue_annot)))
+  
+  fcbf_output = fcbf(mock_expression_table, mock_cell_class, n_genes = 5)
+  
+  expect_equal(rownames(fcbf_output), c("A","B"))
+  expect_equal(fcbf_output["SU"][,1][1], 1)
+  
 })
 
 
 
 test_that("base entropy functions work", {
+  
   expect_error(entropy(0.5))
   expect_error(entropy(as.character(c("A", "A","B", "B"))))
   expect_equivalent(entropy(as.factor(c("A", "A","B", "B"))), 
